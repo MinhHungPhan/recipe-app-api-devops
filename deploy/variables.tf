1464edfe9f668484f87de1bf0e4ea8e743b0e5c5
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-05-14 10:59:36 -0700

    added project and contact variables

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -1,3 +1,11 @@
 variable "prefix" {
     default = "raad" #raad = recipe-app-api-devops
 }
+
+variable "project" {
+  default = "recipe-app-api-devops"
+}
+
+variable "contact" {
+  default = "email@kientree.com"
+}
