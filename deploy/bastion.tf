edc9dd88579e49ff0047cf7bf0eff23c243f78a4
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-05-14 10:25:11 -0700

    added tags for the bastion instance in the bastion.tf file

--- deploy/bastion.tf
+++ deploy/bastion.tf
@@ -12,4 +12,7 @@ data "aws_ami" "amazon_linux" {
 resource "aws_instance" "bastion" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
+  tags = {
+    Name = "${local.prefix}-bastion"
+  }
 }
