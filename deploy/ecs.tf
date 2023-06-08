fdf64cf6e8ef5691b3d342b7808645c0e3be2061
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-06-08 07:09:41 -0700

    added S3 bucket permissions for ECS

--- deploy/ecs.tf
+++ deploy/ecs.tf
@@ -50,6 +50,8 @@ data "template_file" "api_container_definitions" {
     log_group_name    = aws_cloudwatch_log_group.ecs_task_logs.name
     log_group_region  = data.aws_region.current.name
     allowed_hosts     = aws_lb.api.dns_name
+    s3_storage_bucket_name   = aws_s3_bucket.app_public_files.bucket
+    s3_storage_bucket_region = data.aws_region.current.name
   }
 }
 
@@ -125,4 +127,25 @@ resource "aws_ecs_service" "api" {
         container_name   = "proxy"
         container_port   = 8000
     }
+}
+
+data "template_file" "ecs_s3_write_policy" {
+  template = file("./templates/ecs/s3-write-policy.json.tpl")
+
+  vars = {
+    bucket_arn = aws_s3_bucket.app_public_files.arn
+  }
+}
+
+resource "aws_iam_policy" "ecs_s3_access" {
+  name        = "${local.prefix}-AppS3AccessPolicy"
+  path        = "/"
+  description = "Allow access to the recipe app S3 bucket"
+
+  policy = data.template_file.ecs_s3_write_policy.rendered
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_s3_access" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = aws_iam_policy.ecs_s3_access.arn
 }
